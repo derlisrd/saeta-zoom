0d388ca48a6523cc9a5dfcc3a7319bb2f8cc50e6
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-09-12 11:32:24 -0400

    ignorar archivos

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import MenuProvider from "./Components/Menu/MenuProvider";
 import Pages from "./Pages";
 
 function App() {
-  return (<MenuProvider><Pages /></MenuProvider>  );
+  return (
+    <MenuProvider>
+      <Pages />
+    </MenuProvider>
+  );
 }
 
 export default App;
