1f13e6b7fb12cb2a16eafd8f3feee10f18ffd1b5
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-10-09 10:28:48 -0300

    versionado

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,19 +1,14 @@
-
-import { Box,  Typography } from "@mui/material";
-
+import { Box, Typography } from "@mui/material";
 
 function Home() {
-    return (<Box sx={{ display:'flex',alignItems:'center',justifyContent:'center',gap:'8px',flexDirection:'column' }}>
-
-                    <span role="img" style={{ fontSize:'10rem' }} ></span>
-                <Typography variant="h3">
-                    ZOOM OPTICAL
-                </Typography>
-                <Typography variant="h3">
-                    Sistema de Gestion Administrativo
-                </Typography>
-
-    </Box>);
+  return (
+    <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", gap: "8px", flexDirection: "column" }}>
+      <span role="img" style={{ fontSize: "10rem" }}></span>
+      <Typography variant="h3">ZOOM OPTICAL</Typography>
+      <Typography variant="h1">SGA</Typography>
+      <Typography variant="body">1.0.2</Typography>
+    </Box>
+  );
 }
 
 export default Home;
