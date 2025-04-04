685822d41c465bb8750afe51c6edc04655cfd050
Derlis Ruiz Diaz <65002111+derlisrd@users.noreply.github.com>
2025-04-04 14:56:33 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,25 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'site', 
+  build:{
+    outDir: './site',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'utils-vendor': [
+            'axios', 
+            'react-to-print',
+            'sweetalert',
+            'react-number-format',
+            '@mui/material'
+          ]
+        },
+        chunkFileNames: "js/[name].[hash].js",
+        entryFileNames: "assets/[name].[hash].js",
+        assetFileNames: "assets/[name].[hash].[ext]",
+      },
+    }
   },
 })
